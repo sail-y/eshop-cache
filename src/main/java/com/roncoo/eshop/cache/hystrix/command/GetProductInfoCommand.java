94a095f95f016d6d138bee49e6d4bfbac3c77ffb
yangfan <hyyangfan@gmail.com>
2018-07-02 23:00:12 +0800

    源服务资源隔离

---
+++ src/main/java/com/roncoo/eshop/cache/hystrix/command/GetProductInfoCommand.java
@@ -0,0 +1,44 @@
+package com.roncoo.eshop.cache.hystrix.command;
+
+import com.alibaba.fastjson.JSONObject;
+import com.netflix.hystrix.*;
+import com.roncoo.eshop.cache.model.ProductInfo;
+
+/**
+ * @author yangfan
+ * @date 2018/07/02
+ */
+public class GetProductInfoCommand extends HystrixCommand<ProductInfo> {
+
+
+    private Long productId;
+
+    public GetProductInfoCommand(Long productId) {
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("ProductInfoService"))
+                .andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey("GetProductInfoPool"))
+                .andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter()
+                        .withCoreSize(10)
+                        .withMaxQueueSize(12)
+                        .withQueueSizeRejectionThreshold(8)
+                        .withMaximumSize(30)
+                        .withAllowMaximumSizeToDivergeFromCoreSize(true)
+                        .withKeepAliveTimeMinutes(1)
+                        .withMaxQueueSize(50)
+                        .withQueueSizeRejectionThreshold(100))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        // 多少个请求以上才会判断断路器是否需要开启。
+                        .withCircuitBreakerRequestVolumeThreshold(30)
+                        // 错误的请求达到40%的时候就开始断路。
+                        .withCircuitBreakerErrorThresholdPercentage(40)
+                        // 3秒以后尝试恢复
+                        .withCircuitBreakerSleepWindowInMilliseconds(4000))
+        );
+        this.productId = productId;
+    }
+
+    @Override
+    protected ProductInfo run() throws Exception {
+        String productInfoJSON = "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:01:00\"}";
+        return JSONObject.parseObject(productInfoJSON, ProductInfo.class);
+    }
+}
--- src/main/java/com/roncoo/eshop/cache/hystrix/command/GetProductInfoFromRedisCacheCommand.java
+++ src/main/java/com/roncoo/eshop/cache/hystrix/command/GetProductInfoFromRedisCacheCommand.java
@@ -3,6 +3,7 @@ package com.roncoo.eshop.cache.hystrix.command;
 import com.alibaba.fastjson.JSONObject;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
 import com.roncoo.eshop.cache.model.ProductInfo;
 import com.roncoo.eshop.cache.spring.SpringContext;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -18,8 +19,13 @@ public class GetProductInfoFromRedisCacheCommand extends HystrixCommand<ProductI
     private Long productId;
 
     public GetProductInfoFromRedisCacheCommand(Long productId) {
-
-        super(HystrixCommandGroupKey.Factory.asKey("RedisGroup"));
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("RedisGroup"))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        .withExecutionTimeoutInMilliseconds(100)
+                        .withCircuitBreakerRequestVolumeThreshold(1000)
+                        .withCircuitBreakerErrorThresholdPercentage(70)
+                        .withCircuitBreakerSleepWindowInMilliseconds(60 * 1000))
+        );
         this.productId = productId;
     }
 
